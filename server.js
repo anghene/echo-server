3831d8111108265abf1021e0293f3ecac4237041
ava <vlad.anghene@gmail.com>
2019-06-11 21:16:01 +0300

    simplified route to /

--- server.js
+++ server.js
@@ -1,15 +1,12 @@
 var express = require('express');
 var http = require('http');
-var bodyParser = require('body-parser');
 
 var app = express();
-app.use(bodyParser.text({
-  type: function(req) {
-    return 'text';
-  }
-}));
 
-app.get('/post', function (req, res) {
+const bodyParser = require('body-parser');
+app.use(bodyParser);
+
+app.get('/', function (req, res) {
   console.log(req.body);
   res = res.status(200);
   if (req.get('Content-Type')) {
@@ -19,14 +16,14 @@ app.get('/post', function (req, res) {
   res.send(req.body);
 });
 
-app.post('/post', function (req, res) {
+app.post('/', function (req, res) {
   console.log(req.body);
   res = res.status(200);
   if (req.get('Content-Type')) {
-    console.log("Content-Type: " + req.get('Content-Type'));
+    console.log("Content-Type:" + req.get('Content-Type'));
     res = res.type(req.get('Content-Type'));
   }
-  res.send(req.body);
+  
 });
 
 http.createServer(app).listen(7000);
